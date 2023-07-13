3b92cfeed2322b6febbdb5c6e8f0353f8b438149
Juan Diego Mejia Maestre <mejiamaestrejuandiego@gmail.com>
2023-07-12 19:08:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.layouts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.layouts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var  binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btnConstraint.setOnClickListener {
+                startActivity(Intent(this@MainActivity, ConstraintLayoutActivity::class.java))
+            }
+        }
+    }
+}
